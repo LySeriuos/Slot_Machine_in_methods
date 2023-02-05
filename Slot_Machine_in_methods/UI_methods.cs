7efa76c1b5f3c5fd5a602ae915aa415222fbf800
LySeriuos <drungilas.p@gmail.com>
2023-02-05 20:27:51 +0100

    created method for counting win and loses

--- Slot_Machine_in_methods/UI_methods.cs
+++ Slot_Machine_in_methods/UI_methods.cs
@@ -131,7 +131,109 @@
             }
         }
 
-     
+        public static int CoutnWinAndLoses(int[,] array2Dimmensional, int playersChoseOptionToPLay, int playersGameMoney)
+        {
+            int fColFLine = array2Dimmensional[0, 0]; // getting numbers from 2D array 
+            int sColFLine = array2Dimmensional[0, 1];
+            int tColFLine = array2Dimmensional[0, 2];
+            int fColSLine = array2Dimmensional[1, 0];
+            int sColSLine = array2Dimmensional[1, 1];
+            int tColSLine = array2Dimmensional[1, 2];
+            int fColTLine = array2Dimmensional[2, 0];
+            int sColTLine = array2Dimmensional[2, 1];
+            int tColTLine = array2Dimmensional[2, 2];
+
+            // boolean for if statements with horizontal lines
+
+            bool firstLineH = fColFLine == sColFLine && sColFLine == tColFLine;
+            bool secondLineH = fColSLine == sColSLine && sColSLine == tColSLine;
+            bool thirdLineH = fColTLine == sColTLine && sColTLine == tColTLine;
+
+            // boolean for if statements with vertical lines
+
+            bool firstColumnV = fColFLine == fColSLine && fColSLine == fColTLine;
+            bool secondColumnV = sColFLine == sColSLine && sColSLine == sColTLine;
+            bool thirdColumnV = tColFLine == tColSLine && tColSLine == tColTLine;
+
+            // boolean for diagonals
+
+            bool diagonalDown = fColFLine == sColSLine && sColSLine == tColTLine;
+            bool diagonalUp = fColTLine == sColSLine && sColSLine == tColFLine;
+
+            // IF statements for every option
+
+            Console.WriteLine(); // empty line
+
+            int winAmount = 0;
+
+            if (playersChoseOptionToPLay == 1 && playersGameMoney > 0) // player chose 1st option and player must have 1 dollar to play this line
+            {
+                if (secondLineH) // to get this true, all numbers in the middle line in the grid must match
+                {
+                    winAmount = 2; // adding 2 dollars to current balance for user
+                }
+                else
+                {
+                    winAmount--; // taking 1 dollar from current balance because player lost 
+                }
+            }
+            else if (playersChoseOptionToPLay == 2 && playersGameMoney > 2) // player chose 2st option and player must have 3 dollars to play this line
+            {
+                if (firstLineH && secondLineH && thirdLineH) // Most unlikely wining condition on the top to avoid other conditions will break the code
+                {
+                    winAmount = 6;
+
+                }
+                else if (firstLineH && secondLineH || firstLineH && thirdLineH || secondLineH && thirdLineH)
+                {
+                    winAmount = 4;
+                }
+                else if (firstLineH | secondLineH | thirdLineH)
+                {
+                    winAmount = 2;
+                }
+                else
+                {
+                    winAmount -= 3;
+                }
+            }
+            else if (playersChoseOptionToPLay == 3 && playersGameMoney > 2)
+            {
+                // here player plays with columns instead of lines
+                if (firstColumnV && secondColumnV && thirdColumnV)
+                {
+                    winAmount = 6;
+                }
+                else if (firstColumnV && secondColumnV || firstColumnV && thirdColumnV || secondColumnV && thirdColumnV)
+                {
+                    winAmount = 4;
+                }
+                else if (firstColumnV | secondColumnV | thirdColumnV)
+                {
+                    winAmount = 4;
+                }
+                else
+                {
+                    winAmount -= 3;
+                }
+            }
+            else if (playersChoseOptionToPLay == 4 && playersGameMoney > 1)
+            {
+                if (diagonalDown && diagonalUp)
+                {
+                    winAmount = 4;
+                }
+                else if (diagonalDown || diagonalUp)
+                {
+                    winAmount = 2;
+                }
+                else
+                {
+                    winAmount -= 2;
+                }
+            }
+            return winAmount;
+        }
 
         public static int SumUpWinsAndLoses(int num1, int num2)
         {
