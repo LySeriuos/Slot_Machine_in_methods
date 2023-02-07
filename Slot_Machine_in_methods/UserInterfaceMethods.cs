d4eb9ff97604e9ae4c6d37df69dc750c653b79a9
LySeriuos <drungilas.p@gmail.com>
2023-02-07 11:20:17 +0100

    added methods description to UI class

--- Slot_Machine_in_methods/UserInterfaceMethods.cs
+++ Slot_Machine_in_methods/UserInterfaceMethods.cs
@@ -2,6 +2,9 @@
 {
     public static class UserInterfaceMethods
     {
+        /// <summary>
+        /// UI method to print out all the game rools to the screen
+        /// </summary>
         public static void DisplayingRoolsForUser()
         {
             Console.WriteLine("Welcome to the slot mashine");
@@ -18,7 +21,10 @@
                 "4. Diagonals.");
             Console.WriteLine("Good luck!");
         }
-
+        /// <summary>
+        /// Talking in user input as gameMoney and checking if the input is number or is not equal to zero
+        /// </summary>
+        /// <returns>int gameMoney from valid user input</returns>
         public static int UsersGameMoneyFromInput()
         {
             while (true)
@@ -35,8 +41,11 @@
                     continue;
                 }
             }
-        }     
-
+        }
+        /// <summary>
+        /// Talking in user input as chosed game Option from the menu and checking if the input is number or is not equal to zero
+        /// </summary>
+        /// <returns>int playersChoseOptionToPLay from valid user input</returns>
         public static int UsersChosedOption()
         {
             while (true)
@@ -56,31 +65,48 @@
                 }
             }
         }
-
+        /// <summary>
+        /// taking user input as num1 and printing out error message after user's chosed option not exist
+        /// </summary>
+        /// <param name="num1">int playersChoseOptionToPLay used as parameter to method</param>
         public static void InformUserAboutWrongOption(int num1)
         {
             Console.WriteLine($"There is no Option with number {num1}");
         }
 
+        /// <summary>
+        /// Printing error message to inform that game money is not enough to play chosed option
+        /// </summary>
         public static void PrintUserMoneyBalanceTooLow()
         {
             Console.WriteLine("Your current balance is too low! Choose another option!");
         }
 
+        /// <summary>
+        /// Printing out game money balance to user
+        /// </summary>
+        /// <param name="num2">int playersGameMoney used as parameter to method</param>
         public static void PrintTheBalance(int num2)
         {
             Console.WriteLine($"Your acount balance now is: {num2} dollars");
             return;
         }
 
+        /// <summary>
+        /// Printing out message to inform user that he lost
+        /// </summary>
         public static void YouLostMessage()
         {
             Console.WriteLine("You lost! Try again!");
         }
 
-        public static void YouWonMoneyMessage(int countedWinsLoses)
+        /// <summary>
+        /// Printing out message with user winnings 
+        /// </summary>
+        /// <param name="num3">Taking winAmount as parameter</param>
+        public static void YouWonMoneyMessage(int num3)
         {
-            Console.WriteLine($"You won {countedWinsLoses} dollars");
+            Console.WriteLine($"You won {num3} dollars");
         }
     }
 }
